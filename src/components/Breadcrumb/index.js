632b9d467f92a2f9442921f04c0345c1b9d4b23d
newton imonjirie <newtonimonjirie@gmail.com>
2021-01-26 19:52:14 +0100

    completed breadcrumb

---
+++ src/components/Breadcrumb/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Breadcrumb() {
+  return (
+    <div>
+      <p>
+        Divisions{" "}
+        <i
+          style={{
+            color: "#0A5685",
+          }}
+          class="fa fa-angle-right font-weight-bold mx-2"
+          aria-hidden="true"
+        ></i>
+        Module
+      </p>
+    </div>
+  );
+}
