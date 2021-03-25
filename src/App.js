cccbf043e96b3e2218316b7aded949cff44e0090
newton imonjirie <newtonimonjirie@gmail.com>
2021-03-25 21:35:13 +0100

    pangaea test

--- src/App.js
+++ src/App.js
@@ -1,31 +1,61 @@
-import Navbar from "./components/Navbar";
-import Dashboard from "./containers/Dashboard";
+import React, { useState, useEffect } from "react";
+import Header from "./components/Header";
+import Products from "./containers/Products";
+import ProductItem from "./components/ProductItem";
 import "./App.css";
 
-import asbReducer from "./store/reducers/asb";
-import { Provider } from "react-redux";
-import { createStore, applyMiddleware, combineReducers, compose } from "redux";
-import ReduxThunk from "redux-thunk";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql,
+} from "@apollo/client";
 
-const rootReducer = combineReducers({
-  asb: asbReducer,
+const client = new ApolloClient({
+  uri: "https://pangaea-interviews.now.sh/api/graphql",
+  cache: new InMemoryCache(),
 });
 
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const store = createStore(
-  rootReducer,
-  composeEnhancer(applyMiddleware(ReduxThunk))
-);
-
 function App() {
+  const GetProducts = () => {
+    const { loading, error, data } = useQuery(gql`
+      {
+        products {
+          id
+          title
+          image_url
+          price(currency: USD)
+        }
+      }
+    `);
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return console.log(error);
+    // if (error) return <p>Error :(</p>;
+
+    return data.products.map((product, i) => (
+      <ProductItem
+        key={i}
+        _id={product.id}
+        _image={product.image_url}
+        _title={product.title}
+        _price={product.price}
+      />
+    ));
+  };
+
   return (
-    <Provider store={store}>
+    <ApolloProvider client={client}>
       <div className="App">
-        <Navbar />
-        <Dashboard />
+        <Header />
+        <div className="products">
+          <div className="row main-holder">
+            <GetProducts />
+          </div>
+        </div>
       </div>
-    </Provider>
+    </ApolloProvider>
   );
 }
 
